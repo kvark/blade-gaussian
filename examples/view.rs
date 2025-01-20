635448c3004281a86d47b4d2cd900a18b26c79ef
Dzmitry Malyshau <kvark@fastmail.com>
2025-01-19 19:14:28 -0800

    Hook up swapchain to the example

--- examples/view.rs
+++ examples/view.rs
@@ -277,103 +277,204 @@ struct DrawData {
     g_acc_struct: gpu::AccelerationStructure,
 }
 
+struct Example {
+    draw_pipeline: gpu::RenderPipeline,
+    command_encoder: gpu::CommandEncoder,
+    prev_sync_point: Option<gpu::SyncPoint>,
+    window_size: winit::dpi::PhysicalSize<u32>,
+    point_cloud: PointCloud,
+    surface: gpu::Surface,
+    context: gpu::Context,
+}
+
+impl Example {
+    fn make_surface_config(size: winit::dpi::PhysicalSize<u32>) -> gpu::SurfaceConfig {
+        log::info!("Window size: {:?}", size);
+        gpu::SurfaceConfig {
+            size: gpu::Extent {
+                width: size.width,
+                height: size.height,
+                depth: 1,
+            },
+            usage: gpu::TextureUsage::TARGET,
+            display_sync: gpu::DisplaySync::Recent,
+            ..Default::default()
+        }
+    }
+
+    fn init(window: &winit::window::Window) -> Self {
+        let context = unsafe {
+            gpu::Context::init(gpu::ContextDesc {
+                presentation: true,
+                validation: cfg!(debug_assertions),
+                timing: false,
+                capture: false,
+                overlay: true,
+                device_id: 0,
+            })
+            .unwrap()
+        };
+        println!("{:?}", context.device_information());
+        let window_size = window.inner_size();
+
+        let surface = context
+            .create_surface_configured(window, Self::make_surface_config(window_size))
+            .unwrap();
+        let info = surface.info();
+
+        let shader = {
+            let source = std::fs::read_to_string("examples/shader.wgsl").unwrap();
+            context.create_shader(gpu::ShaderDesc { source: &source })
+        };
+        let draw_layout = <DrawData as gpu::ShaderData>::layout();
+        let draw_pipeline = context.create_render_pipeline(gpu::RenderPipelineDesc {
+            name: "main",
+            data_layouts: &[&draw_layout],
+            primitive: gpu::PrimitiveState {
+                topology: gpu::PrimitiveTopology::TriangleStrip,
+                ..Default::default()
+            },
+            vertex: shader.at("draw_vs"),
+            vertex_fetches: &[],
+            fragment: Some(shader.at("draw_fs")),
+            color_targets: &[info.format.into()],
+            depth_stencil: None,
+            multisample_state: Default::default(),
+        });
+
+        let mut command_encoder = context.create_command_encoder(gpu::CommandEncoderDesc {
+            name: "main",
+            buffer_count: 2,
+        });
+
+        let arg_name = env::args()
+            .nth(1)
+            .expect("Need a path to .spz as an argument");
+        let point_cloud = PointCloud::load(&arg_name, &context, &mut command_encoder);
+
+        Self {
+            draw_pipeline,
+            command_encoder,
+            prev_sync_point: None,
+            window_size,
+            point_cloud,
+            surface,
+            context,
+        }
+    }
+
+    fn deinit(&mut self) {
+        self.wait_for_gpu();
+        self.context
+            .destroy_render_pipeline(&mut self.draw_pipeline);
+        self.context
+            .destroy_command_encoder(&mut self.command_encoder);
+        self.context.destroy_surface(&mut self.surface);
+        self.point_cloud.deinit(&self.context);
+    }
+
+    fn resize(&mut self, size: winit::dpi::PhysicalSize<u32>) {
+        self.window_size = size;
+        let config = Self::make_surface_config(size);
+        self.context.reconfigure_surface(&mut self.surface, config);
+    }
+
+    fn wait_for_gpu(&mut self) {
+        if let Some(sp) = self.prev_sync_point.take() {
+            self.context.wait_for(&sp, !0);
+        }
+    }
+
+    fn render(&mut self) {
+        if self.window_size == Default::default() {
+            return;
+        }
+        let frame = self.surface.acquire_frame();
+
+        self.command_encoder.start();
+        self.command_encoder.init_texture(frame.texture());
+
+        if let mut pass = self.command_encoder.render(
+            "main",
+            gpu::RenderTargetSet {
+                colors: &[gpu::RenderTarget {
+                    view: frame.texture_view(),
+                    init_op: gpu::InitOp::Clear(gpu::TextureColor::OpaqueBlack),
+                    finish_op: gpu::FinishOp::Store,
+                }],
+                depth_stencil: None,
+            },
+        ) {
+            let mut pen = pass.with(&self.draw_pipeline);
+            pen.bind(
+                0,
+                &DrawData {
+                    g_parameters: Parameters {
+                        cam_position: [0.0, 0.0, 0.0],
+                        depth: 1000.0,
+                        cam_orientation: [0.0, 0.0, 0.0, 1.0],
+                        fov: [0.7, 0.7],
+                        pad: [0; 2],
+                    },
+                    g_acc_struct: self.point_cloud.tlas,
+                },
+            );
+            pen.draw(0, 3, 0, 1);
+        }
+        self.command_encoder.present(frame);
+        let sync_point = self.context.submit(&mut self.command_encoder);
+
+        self.wait_for_gpu();
+        self.prev_sync_point = Some(sync_point);
+    }
+}
 fn main() {
     env_logger::init();
-    log::info!("Initializing");
-    let context = unsafe {
-        gpu::Context::init(gpu::ContextDesc {
-            validation: !cfg!(debug_assertions),
-            ..Default::default()
-        })
-    }
-    .unwrap();
-    let mut command_encoder = context.create_command_encoder(gpu::CommandEncoderDesc {
-        name: "init",
-        buffer_count: 1,
-    });
 
-    let arg_name = env::args()
-        .nth(1)
-        .expect("Need a path to .spz as an argument");
-    let mut point_cloud = PointCloud::load(&arg_name, &context, &mut command_encoder);
+    let event_loop = winit::event_loop::EventLoop::new().unwrap();
+    let window_attributes =
+        winit::window::Window::default_attributes().with_title("blade-gaussian-viewer");
+    let window = event_loop.create_window(window_attributes).unwrap();
 
-    let extent = gpu::Extent {
-        width: 100,
-        height: 100,
-        depth: 1,
-    };
-    let target_format = gpu::TextureFormat::Rgba8Unorm;
-    let texture = context.create_texture(gpu::TextureDesc {
-        name: "target",
-        format: target_format,
-        size: extent,
-        array_layer_count: 1,
-        mip_level_count: 1,
-        sample_count: 1,
-        dimension: gpu::TextureDimension::D2,
-        usage: gpu::TextureUsage::TARGET,
-    });
-    let texture_view = context.create_texture_view(
-        texture,
-        gpu::TextureViewDesc {
-            name: "target",
-            format: target_format,
-            dimension: gpu::ViewDimension::D2,
-            subresources: &Default::default(),
-        },
-    );
+    let mut example = Example::init(&window);
 
-    let source = std::fs::read_to_string("examples/shader.wgsl").unwrap();
-    let shader = context.create_shader(gpu::ShaderDesc { source: &source });
-    let draw_layout = <DrawData as gpu::ShaderData>::layout();
-    let draw_pipeline = context.create_render_pipeline(gpu::RenderPipelineDesc {
-        name: "main",
-        data_layouts: &[&draw_layout],
-        primitive: gpu::PrimitiveState {
-            topology: gpu::PrimitiveTopology::TriangleStrip,
-            ..Default::default()
-        },
-        vertex: shader.at("draw_vs"),
-        vertex_fetches: &[],
-        fragment: Some(shader.at("draw_fs")),
-        color_targets: &[target_format.into()],
-        depth_stencil: None,
-        multisample_state: Default::default(),
-    });
-
-    command_encoder.start();
-    if let mut pass = command_encoder.render(
-        "main",
-        gpu::RenderTargetSet {
-            colors: &[gpu::RenderTarget {
-                view: texture_view,
-                init_op: gpu::InitOp::Clear(gpu::TextureColor::OpaqueBlack),
-                finish_op: gpu::FinishOp::Store,
-            }],
-            depth_stencil: None,
-        },
-    ) {
-        let mut pen = pass.with(&draw_pipeline);
-        pen.bind(
-            0,
-            &DrawData {
-                g_parameters: Parameters {
-                    cam_position: [0.0, 0.0, 0.0],
-                    depth: 1000.0,
-                    cam_orientation: [0.0, 0.0, 0.0, 1.0],
-                    fov: [0.7, 0.7],
-                    pad: [0; 2],
+    event_loop
+        .run(|event, target| {
+            target.set_control_flow(winit::event_loop::ControlFlow::Poll);
+            match event {
+                winit::event::Event::AboutToWait => {
+                    window.request_redraw();
+                }
+                winit::event::Event::WindowEvent { event, .. } => match event {
+                    winit::event::WindowEvent::Resized(size) => {
+                        example.resize(size);
+                    }
+                    winit::event::WindowEvent::KeyboardInput {
+                        event:
+                            winit::event::KeyEvent {
+                                physical_key: winit::keyboard::PhysicalKey::Code(key_code),
+                                state: winit::event::ElementState::Pressed,
+                                ..
+                            },
+                        ..
+                    } => match key_code {
+                        winit::keyboard::KeyCode::Escape => {
+                            target.exit();
+                        }
+                        _ => {}
+                    },
+                    winit::event::WindowEvent::CloseRequested => {
+                        target.exit();
+                    }
+                    winit::event::WindowEvent::RedrawRequested => {
+                        example.render();
+                    }
+                    _ => {}
                 },
-                g_acc_struct: point_cloud.tlas,
-            },
-        );
-        pen.draw(0, 3, 0, 1);
-    }
-    let sync_point = context.submit(&mut command_encoder);
-    context.wait_for(&sync_point, !0);
-    context.destroy_texture_view(texture_view);
-    context.destroy_texture(texture);
+                _ => {}
+            }
+        })
+        .unwrap();
 
-    context.destroy_command_encoder(&mut command_encoder);
-    point_cloud.deinit(&context);
+    example.deinit();
 }
